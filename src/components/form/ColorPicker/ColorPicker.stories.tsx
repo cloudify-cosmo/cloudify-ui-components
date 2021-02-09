6a40b637693eb8c08801dd3324276e0806dc74ae
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-02-09 13:34:12 +0100

    RD-1230 Complete transforming ColorPicker to TS

--- src/components/form/ColorPicker/ColorPicker.stories.tsx
+++ src/components/form/ColorPicker/ColorPicker.stories.tsx
@@ -1,13 +1,27 @@
+import { Story } from '@storybook/react';
 import React from 'react';
 
-import LiveEditDecorator from 'decorators/LiveEditDecorator';
 import Form from '../Form/Form';
+import { ColorPickerProps } from './ColorPicker';
 
 export default {
     title: 'Form/ColorPicker',
     component: Form.ColorPicker,
-    decorators: [LiveEditDecorator({ Form })]
+    argTypes: {
+        value: {
+            control: 'color'
+        }
+    }
 };
 
-export const basic = () => <Form.ColorPicker />;
+// eslint-disable-next-line react/jsx-props-no-spreading
+export const basic: Story<Required<ColorPickerProps>> = args => <Form.ColorPicker {...args} />;
 basic.storyName = 'Default';
+basic.args = {
+    name: '',
+    value: '#000000',
+    onChange: (...args) => {
+        // eslint-disable-next-line no-console
+        console.log('changed', ...args);
+    }
+};
--- src/components/form/ColorPicker/ColorPicker.tsx
+++ src/components/form/ColorPicker/ColorPicker.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { ChangeEvent, FunctionComponent } from 'react';
 
-import { CompactPicker } from 'react-color';
+import { ColorChangeHandler, CompactPicker } from 'react-color';
 import tinycolor from 'tinycolor2';
+
+export interface ColorPickerProps {
+    /**
+     * Name of the color picker component
+     */
+    name?: string;
+
+    /**
+     * Hexadecimal color value
+     */
+    value?: string;
+
+    /**
+     * Function called on color change
+     */
+    onChange?: (event: ChangeEvent<HTMLInputElement>, data: { name: string; value: string }) => void;
+}
 
 /**
  * ColorPicker is a component showing CompactPicker component from [react-color](https://casesandberg.github.io/react-color/) library
@@ -10,35 +26,14 @@ import tinycolor from 'tinycolor2';
  * Accessible as `ColorPicker` or `Form.ColorPicker`.
  *
  */
-export default function ColorPicker(props) {
-    const { name, value, onChange } = props;
-
-    const handleInputChange = (color, event) => {
+const ColorPicker: FunctionComponent<ColorPickerProps> = ({ name = '', value = '#000000', onChange = () => {} }) => {
+    const handleInputChange: ColorChangeHandler = (color, event) => {
         onChange(event, { name, value: color.hex });
     };
 
     const color = tinycolor(value);
 
     return <CompactPicker color={color.toHsl()} onChange={handleInputChange} />;
-}
-
-ColorPicker.propTypes = {
-    /**
-     * name of the color picker component
-     */
-    name: PropTypes.string,
-    /**
-     * hexadecimal color value
-     */
-    value: PropTypes.string,
-    /**
-     * function called on color change
-     */
-    onChange: PropTypes.func
 };
 
-ColorPicker.defaultProps = {
-    name: '',
-    value: '#000000',
-    onChange: () => {}
-};
+export default ColorPicker;
